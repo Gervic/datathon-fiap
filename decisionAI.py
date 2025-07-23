6465ccfb57508ffa55d1e68ea2c07f0294e1c5dc
Victorien Nago <ger.nago@gmail.com>
2025-07-22 21:45:34 -0300

    Update decisionAI.py

--- decisionAI.py
+++ decisionAI.py
@@ -566,7 +566,7 @@ if st.session_state.selected_action == 'analyze_cv':
                     )
                 with st.spinner('Saindo do forno...'):
                     st.session_state.messages.append({"role": "assistant", "content": ranking_message})
-                    st.markdown(st.session_state.messages[:-1]["content"])
+                    st.markdown(st.session_state.messages[-1]["content"])
                     st.rerun() 
 
 #General Chat Input (for "Tirar uma dúvida" or follow-ups)
